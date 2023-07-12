ab2a6bbf389445effddb86bf13964055d52d3cf3
Elison Nascimento <elisonno997@gmail.com>
2023-07-12 15:31:45 -0300

    adding area calculation method
    Adicionado método de cálculo de área na classe triângulo.

--- CalculoTrianguloComPOO/src/main/java/application/Program.java
+++ CalculoTrianguloComPOO/src/main/java/application/Program.java
@@ -29,11 +29,9 @@ public class Program {
         System.out.print("yC: ");
         y.c = sc.nextDouble();
 
-        double p = (x.a + x.b + x.c)/2.0;
-        double areaX = Math.sqrt(p * (p - x.a) * (p - x.a) * (p - x.c));
 
-        p = (y.a + y.b + y.c)/2.0;
-        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+        double areaX = x.area();
+        double areaY = y.area();
 
         System.out.printf("Triangle X area: %.4f\n", areaX);
         System.out.printf("Triangle Y area: %.4f\n", areaY);
