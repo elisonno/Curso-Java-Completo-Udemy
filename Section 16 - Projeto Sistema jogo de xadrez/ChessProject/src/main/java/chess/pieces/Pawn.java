31c487cc02bf60b0e0e00f5b7a2aabde97f6b279
Elison Nascimento <elisonno997@gmail.com>
2023-08-14 17:42:56 -0300

    Jogada especial en passant

--- Section 16 - Projeto Sistema jogo de xadrez/ChessProject/src/main/java/chess/pieces/Pawn.java
+++ Section 16 - Projeto Sistema jogo de xadrez/ChessProject/src/main/java/chess/pieces/Pawn.java
@@ -2,12 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class Pawn extends ChessPiece {
-    public Pawn(Board board, Color color) {
+
+    private ChessMatch chessMatch;
+
+    public Pawn(Board board, Color color, ChessMatch chessMatch) {
         super(board, color);
+        this.chessMatch = chessMatch;
     }
 
     @Override
@@ -34,6 +39,18 @@ public class Pawn extends ChessPiece {
                 possiblePositions[newPosition.getRow()][newPosition.getColumn()] = true;
             }
 
+            //special move en passant white
+            if(position.getRow() == 3){
+                Position left = new Position(position.getRow(), position.getColumn() - 1);
+                if(getBoard().positionExists(left) && isThereOpponentPiece(left) && getBoard().piece(left) == chessMatch.getEnPassantVulnerable()){
+                    possiblePositions[left.getRow() - 1][left.getColumn()] = true;
+                }
+                Position right = new Position(position.getRow(), position.getColumn() + 1);
+                if(getBoard().positionExists(right) && isThereOpponentPiece(right) && getBoard().piece(right) == chessMatch.getEnPassantVulnerable()){
+                    possiblePositions[right.getRow() - 1][right.getColumn()] = true;
+                }
+            }
+
         }
         else{
             newPosition.setValues(position.getRow() + 1, position.getColumn());
@@ -52,6 +69,18 @@ public class Pawn extends ChessPiece {
             newPosition.setValues(position.getRow() + 1, position.getColumn() + 1);
             if(getBoard().positionExists(newPosition) && isThereOpponentPiece(newPosition)){
                 possiblePositions[newPosition.getRow()][newPosition.getColumn()] = true;
+            }
+
+            //special move en passant black
+            if(position.getRow() == 4){
+                Position left = new Position(position.getRow(), position.getColumn() - 1);
+                if(getBoard().positionExists(left) && isThereOpponentPiece(left) && getBoard().piece(left) == chessMatch.getEnPassantVulnerable()){
+                    possiblePositions[left.getRow() + 1][left.getColumn()] = true;
+                }
+                Position right = new Position(position.getRow(), position.getColumn() + 1);
+                if(getBoard().positionExists(right) && isThereOpponentPiece(right) && getBoard().piece(right) == chessMatch.getEnPassantVulnerable()){
+                    possiblePositions[right.getRow() + 1][right.getColumn()] = true;
+                }
             }
         }
 
