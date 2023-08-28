4f31d509f901af05349fa4dc7ce88c6308f8831f
Elison Nascimento <elisonno997@gmail.com>
2023-08-28 11:39:05 -0300

    comandos usados na aula 271

--- section21-acesso-a-banco-de-dados-com-jdbc/database-access-with-jdbc/src/main/java/application/Application.java
+++ section21-acesso-a-banco-de-dados-com-jdbc/database-access-with-jdbc/src/main/java/application/Application.java
@@ -1,36 +1,57 @@
 package application;
 
 import database.DB;
-import database.DbException;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Application {
     public static void main(String[] args) {
-        Connection connection = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
         try {
             connection = DB.getConnection();
+          /*  preparedStatement = connection.prepareStatement(
+                    "INSERT INTO seller "
+                    + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+                    + "Values "
+                    + "(?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
 
-            statement = connection.createStatement();
+            preparedStatement.setString(1, "Gerome Smith");
+            preparedStatement.setString(2, "gerome@gmail.com");
+            preparedStatement.setDate(3, new java.sql.Date(sdf.parse("22/07/1995").getTime()));
+            preparedStatement.setDouble(4, 3500.0);
+            preparedStatement.setInt(5, 2);*/
 
-            resultSet = statement.executeQuery("select * from department");
+            preparedStatement = connection.prepareStatement(
+                    "insert into department (Name) values ('D1'),('D2')",
+                    Statement.RETURN_GENERATED_KEYS);
 
-            while (resultSet.next()) {
-                System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Name"));
+            int rowsAffected = preparedStatement.executeUpdate();
+            if(rowsAffected > 0){
+                ResultSet resultSet = preparedStatement.getGeneratedKeys();
+                while (resultSet.next()){
+                    int id = resultSet.getInt(1);
+                    System.out.println("Done! Id = "+ id);
+                }
             }
-        } catch (SQLException e) {
+            else {
+                System.out.println("No rows affected!");
+            }
+        }
+        catch (SQLException e){
             e.printStackTrace();
-        } finally {
-            DB.closeResulSet(resultSet);
-            DB.closeStatement(statement);
+        }/*
+        catch (ParseException e){
+            e.printStackTrace();
+        }*/
+        finally {
+            DB.closeStatement(preparedStatement);
             DB.closeConnection();
-
         }
     }
 }
