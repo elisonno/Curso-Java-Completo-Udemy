55091f9dc4e616e7aceb74f57d320a985abfec64
Elison Nascimento <elisonno997@gmail.com>
2023-08-28 11:06:07 -0300

    Comando utilizados na aula 270

---
+++ section21-acesso-a-banco-de-dados-com-jdbc/database-access-with-jdbc/src/main/java/application/Application.java
@@ -0,0 +1,36 @@
+package application;
+
+import database.DB;
+import database.DbException;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Application {
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DB.getConnection();
+
+            statement = connection.createStatement();
+
+            resultSet = statement.executeQuery("select * from department");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeResulSet(resultSet);
+            DB.closeStatement(statement);
+            DB.closeConnection();
+
+        }
+    }
+}
