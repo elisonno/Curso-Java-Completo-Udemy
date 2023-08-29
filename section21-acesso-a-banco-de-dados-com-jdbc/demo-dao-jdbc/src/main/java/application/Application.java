e6f27bbd514d7bb9bee808b8b354fc53a549ab85
Elison Nascimento <elisonno997@gmail.com>
2023-08-29 05:05:58 -0300

    Implementando SellerDaoJDBC e DaoFactory

--- section21-acesso-a-banco-de-dados-com-jdbc/demo-dao-jdbc/src/main/java/application/Application.java
+++ section21-acesso-a-banco-de-dados-com-jdbc/demo-dao-jdbc/src/main/java/application/Application.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,7 @@ public class Application {
 
         Department books = new Department(1, "Books");
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0,books );
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(seller);
     }
 }
