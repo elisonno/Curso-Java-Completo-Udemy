98deaa3a19ddde732c608516cbfe61f0c608e82a
Elison Nascimento <elisonno997@gmail.com>
2023-08-29 02:16:03 -0300

    Adicionando a classe de vendedor

--- section21-acesso-a-banco-de-dados-com-jdbc/demo-dao-jdbc/src/main/java/application/Application.java
+++ section21-acesso-a-banco-de-dados-com-jdbc/demo-dao-jdbc/src/main/java/application/Application.java
@@ -1,11 +1,15 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Application {
     public static void main(String[] args) {
 
         Department books = new Department(1, "Books");
-        System.out.println(books);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0,books );
+        System.out.println(seller);
     }
 }
