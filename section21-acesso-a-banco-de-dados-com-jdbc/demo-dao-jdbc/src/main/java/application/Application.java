d342f4b856af3303cff85b4821131439edaf93ce
Elison Nascimento <elisonno997@gmail.com>
2023-08-30 02:16:52 -0300

    Adicionando o método update

--- section21-acesso-a-banco-de-dados-com-jdbc/demo-dao-jdbc/src/main/java/application/Application.java
+++ section21-acesso-a-banco-de-dados-com-jdbc/demo-dao-jdbc/src/main/java/application/Application.java
@@ -15,8 +15,8 @@ public class Application {
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("--- TEST 1: seller findById ---");
-        Seller sellerTest1 = sellerDao.findById(3);
-        System.out.println(sellerTest1);
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
 
         System.out.println("\n--- TEST 2: seller findByDepartment ---");
         Department departmentTest2 = new Department(2, null);
@@ -31,9 +31,17 @@ public class Application {
             System.out.println(sellerTest3);
         }
 
-        System.out.println("\n--- TEST 4: seller insert ---");
+        /*System.out.println("\n--- TEST 4: seller insert ---");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, departmentTest2);
         sellerDao.insert(newSeller);
-        System.out.println("Inserted! New id = " + newSeller.getId());
+        System.out.println("Inserted! New id = " + newSeller.getId());*/
+
+        System.out.println("\n--- TEST 5: seller update ---");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Wayne");
+        seller.setEmail("martha@gmail.com");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
+        System.out.println(seller);
     }
 }
