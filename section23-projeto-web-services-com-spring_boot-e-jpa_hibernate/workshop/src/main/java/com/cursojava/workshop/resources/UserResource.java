33ad14be4b499f109e744f254f57ef8991a771ee
Elison Nascimento <elisonno997@gmail.com>
2023-08-31 20:01:13 -0300

    User entity and resource

---
+++ section23-projeto-web-services-com-spring_boot-e-jpa_hibernate/workshop/src/main/java/com/cursojava/workshop/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.cursojava.workshop.resources;
+
+import com.cursojava.workshop.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User user = new User(1L, "maria","maria@gmail.com", "99999999","123456");
+        return ResponseEntity.ok().body(user);
+    }
+}
