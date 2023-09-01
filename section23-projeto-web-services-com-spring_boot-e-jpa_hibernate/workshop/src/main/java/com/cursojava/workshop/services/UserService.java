8ec74913490c764544e7dca6af5fd79630f6725f
Elison Nascimento <elisonno997@gmail.com>
2023-08-31 21:42:11 -0300

    Service layer, component registration

---
+++ section23-projeto-web-services-com-spring_boot-e-jpa_hibernate/workshop/src/main/java/com/cursojava/workshop/services/UserService.java
@@ -0,0 +1,25 @@
+package com.cursojava.workshop.services;
+
+import com.cursojava.workshop.entities.User;
+import com.cursojava.workshop.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> user = userRepository.findById(id);
+        return user.get();
+    }
+}
