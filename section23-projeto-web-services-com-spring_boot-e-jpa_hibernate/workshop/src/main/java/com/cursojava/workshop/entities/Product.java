08f1ac5ab457d378877a3175562126b80627e1a9
Elison Nascimento <elisonno997@gmail.com>
2023-09-04 01:16:48 -0300

    Product-OrderItem one-to-many association

--- section23-projeto-web-services-com-spring_boot-e-jpa_hibernate/workshop/src/main/java/com/cursojava/workshop/entities/Product.java
+++ section23-projeto-web-services-com-spring_boot-e-jpa_hibernate/workshop/src/main/java/com/cursojava/workshop/entities/Product.java
@@ -1,5 +1,6 @@
 package com.cursojava.workshop.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -25,6 +26,9 @@ public class Product implements Serializable {
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -79,6 +83,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrder() {
+        Set<Order> orderSet = new HashSet<>();
+        for (OrderItem x : items){
+            orderSet.add(x.getOrder());
+        }
+        return orderSet;
     }
 
     @Override
