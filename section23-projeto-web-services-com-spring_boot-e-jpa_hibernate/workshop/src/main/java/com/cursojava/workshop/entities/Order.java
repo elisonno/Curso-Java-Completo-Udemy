a59d86736bd6ffc031d65a59ff3f63031efc565d
Elison Nascimento <elisonno997@gmail.com>
2023-09-04 01:34:50 -0300

    Payment, one-to-one association

--- section23-projeto-web-services-com-spring_boot-e-jpa_hibernate/workshop/src/main/java/com/cursojava/workshop/entities/Order.java
+++ section23-projeto-web-services-com-spring_boot-e-jpa_hibernate/workshop/src/main/java/com/cursojava/workshop/entities/Order.java
@@ -2,7 +2,6 @@ package com.cursojava.workshop.entities;
 
 import com.cursojava.workshop.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -31,6 +30,9 @@ public class Order implements Serializable {
 
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
+
+    @OneToOne(mappedBy = "order", cascade =  CascadeType.ALL)
+    private Payment payment;
 
     public Order() {
     }
@@ -78,6 +80,14 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
