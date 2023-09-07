0b9a03a716414cc11ba64bae025654e4bbdadacd
Elison Nascimento <elisonno997@gmail.com>
2023-09-06 21:19:21 -0300

    Obtendo um usuário por id

--- section24-projeto-api-restful-com-spring_boot-e-banco-mongo_db-web-services+nosql/workshopmongo/src/main/java/com/cursojava/workshopmongo/resource/UserResource.java
+++ section24-projeto-api-restful-com-spring_boot-e-banco-mongo_db-web-services+nosql/workshopmongo/src/main/java/com/cursojava/workshopmongo/resource/UserResource.java
@@ -5,9 +5,7 @@ import com.cursojava.workshopmongo.dto.UserDTO;
 import com.cursojava.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,11 @@ public class UserResource {
         List<User> list = service.findAll();
         List<UserDTO> listDTO = list.stream().map(UserDTO::new).toList();
         return ResponseEntity.ok().body(listDTO);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
     }
 }
