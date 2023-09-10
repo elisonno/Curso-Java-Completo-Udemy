3bae721696ffecc1f62b5816deda0ebf0820ce60
Elison Nascimento <elisonno997@gmail.com>
2023-09-10 03:47:26 -0300

    Criando entity Post com User aninhado

--- section24-projeto-api-restful-com-spring_boot-e-banco-mongo_db-web-services+nosql/workshopmongo/src/main/java/com/cursojava/workshopmongo/config/Instantiation.java
+++ section24-projeto-api-restful-com-spring_boot-e-banco-mongo_db-web-services+nosql/workshopmongo/src/main/java/com/cursojava/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.cursojava.workshopmongo.config;
 
+import com.cursojava.workshopmongo.domain.Post;
 import com.cursojava.workshopmongo.domain.User;
+import com.cursojava.workshopmongo.repository.PostRepository;
 import com.cursojava.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -14,15 +18,34 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        userRepository.saveAll(Arrays.asList(maria,alex,bob));
+        Post post1 = new Post(null,
+                simpleDateFormat.parse("21/03/2018"),
+                "Partiu viagem",
+                "Vou viajar para São Paulo. Abraços!",
+                maria);
+        Post post2 = new Post(null,
+                simpleDateFormat.parse("23/03/2018"),
+                "Bom dia",
+                "Acordei feliz hoje!",
+                maria);
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1,post2));
     }
 }
